731533978a4cd0b7ab26b9b121b870272443cd3b
posionjoke <posionjoke@gmail.com>
2019-08-07 20:15:58 +0200

    All unused methods in Debtors Interfaces was deleted

--- src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorHistoryRepo.java
+++ src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorHistoryRepo.java
@@ -4,9 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.bykowski.rectangleapp.repositories.repo_struct.DebtorHistory;
 
-import java.util.List;
-
 @Repository
 public interface DebtorHistoryRepo extends CrudRepository<DebtorHistory, Long> {
-    List<DebtorHistory> findByNameAndId(String name, Long ID);
 }
--- src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorRepo.java
+++ src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorRepo.java
@@ -3,12 +3,10 @@ package pl.bykowski.rectangleapp.repositories.repo_interfaces;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.bykowski.rectangleapp.repositories.repo_struct.Debtor;
-import pl.bykowski.rectangleapp.repositories.repo_struct.DebtorDetails;
 
 import java.util.List;
 
 @Repository
 public interface DebtorRepo extends CrudRepository<Debtor, Long> {
     List<Debtor> findByName(String name);
-    List<DebtorDetails> findByNameAndId(String name, Long ID);
 }
