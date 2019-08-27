d3eb942ae1061e76f96f43bd3545a556f46a599a
posionjoke <posionjoke@gmail.com>
2019-08-27 12:14:59 +0200

    refactor DebtorService

--- src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorDetailsRepo.java
+++ src/main/java/pl/bykowski/rectangleapp/repositories/repo_interfaces/DebtorDetailsRepo.java
@@ -6,7 +6,8 @@ import pl.bykowski.rectangleapp.repositories.repo_struct.DebtorDetails;
 import java.util.List;
 
 public interface DebtorDetailsRepo extends CrudRepository<DebtorDetails, Long> {
-    List<DebtorDetails> findByName(String name);
-    List<DebtorDetails> findByNameAndId(String name, Long ID);
+    DebtorDetails findByName(String name);
+
+    DebtorDetails findByNameAndId(String name, Long ID);
     List<DebtorDetails> findByUserName(String userName);
 }
