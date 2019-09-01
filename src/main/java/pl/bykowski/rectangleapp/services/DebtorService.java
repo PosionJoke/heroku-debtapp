dee9ae3f2e9b73463c77b9f07b1484bf37e28db4
posionjoke <posionjoke@gmail.com>
2019-09-01 19:31:46 +0200

    Revert "Merge remote-tracking branch 'origin/spring_security' into spring_security"
    This reverts commit 8f3722d43316acac95726795d265caaab7b382c1, reversing
    changes made to 453d25adce8ad20d0ce7c3a0958fef9c9226eec0.

---
+++ src/main/java/pl/bykowski/rectangleapp/services/DebtorService.java
@@ -0,0 +1,185 @@
+package pl.bykowski.rectangleapp.services;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.bykowski.rectangleapp.repositories.repo_interfaces.DebtorDetailsRepo;
+import pl.bykowski.rectangleapp.repositories.repo_interfaces.DebtorHistoryRepo;
+import pl.bykowski.rectangleapp.repositories.repo_interfaces.DebtorRepo;
+import pl.bykowski.rectangleapp.repositories.repo_struct.Debtor;
+import pl.bykowski.rectangleapp.repositories.repo_struct.DebtorDetails;
+import pl.bykowski.rectangleapp.repositories.repo_struct.DebtorHistory;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@Service
+public class DebtorService {
+
+    private DebtorRepo debtorRepo;
+    private DebtorDetailsRepo debtorDetailsRepo;
+    private DebtorHistoryRepo debtorHistoryRepo;
+
+    public DebtorService(DebtorRepo debtorRepo, DebtorDetailsRepo debtorDetailsRepo, DebtorHistoryRepo debtorHistoryRepo) {
+        this.debtorDetailsRepo = debtorDetailsRepo;
+        this.debtorHistoryRepo = debtorHistoryRepo;
+        this.debtorRepo = debtorRepo;
+    }
+
+    private boolean isThisNameFree(String debtorName, String userName) {
+        List<Debtor> debtorList = debtorRepo.findByUserName(userName);
+
+        for (Debtor debtor : debtorList) {
+            if (debtor.getName().equalsIgnoreCase(debtorName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String findUserName() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        return currentPrincipalName;
+    }
+
+    public String addNewDebt(String debtorName, float debtValue, String reasonForTheDebt, String userName) {
+        String areaInfoValue = "";
+        if (isThisNameFree(debtorName, findUserName())) {
+            addNewDebtor(debtorName, debtValue, reasonForTheDebt, userName);
+            areaInfoValue = (debtorName + " is added! \n Debt value -> " + debtValue);
+        }
+        else {
+            areaInfoValue = addExtraDebt(debtorName, debtValue, userName);
+            addNewDebtorDetails(debtorName, debtValue, reasonForTheDebt, userName);
+//            updateDebtorTotalDebt(debtorName, debtValue);
+        }
+        return areaInfoValue;
+    }
+
+    public void updateDebtorTotalDebt(String debtorName, float debtValue) {
+        Debtor debtor = debtorRepo.findByName(debtorName);
+        float newDebt = debtor.getTotalDebt() + debtValue;
+        debtor.setTotalDebt(newDebt);
+        debtorRepo.save(debtor);
+    }
+
+    public String addExtraDebt(String debtorName, float debtValue, String userName) {
+        Debtor changedDebtor = debtorRepo.findByName(debtorName);
+        float newDebt = debtValue + changedDebtor.getTotalDebt();
+        changedDebtor.setTotalDebt(newDebt);
+        changedDebtor.setUserName(userName);
+        debtorRepo.save(changedDebtor);
+
+        String areaInfoValue = ("New debt of " + changedDebtor.getName() + " \nis equals to " + changedDebtor.getTotalDebt());
+        return areaInfoValue;
+    }
+
+    public void addNewDebtorDetails(String debtorName, float debtValue, String reasonForTheDebt, String userName) {
+        DebtorDetails debtorDetails = new DebtorDetails();
+        debtorDetails.setName(debtorName);
+        debtorDetails.setDebt(debtValue);
+        debtorDetails.setDate(LocalDate.now());
+        debtorDetails.setReasonForTheDebt(reasonForTheDebt);
+        debtorDetails.setUserName(userName);
+
+        debtorDetailsRepo.save(debtorDetails);
+    }
+
+    public String addNewDebtor(String debtorName, float debtValue, String reasonForTheDebt, String userName) {
+        String areaInfoValue = "";
+
+        if (isThisNameFree(debtorName, findUserName())) {
+            Debtor debtor = new Debtor();
+            debtor.setName(debtorName);
+            debtor.setTotalDebt((debtValue + debtor.getTotalDebt()));
+            debtor.setDate(LocalDate.now());
+            debtor.setUserName(userName);
+
+            addNewDebtorDetails(debtorName, debtValue, reasonForTheDebt, userName);
+            debtorRepo.save(debtor);
+
+            areaInfoValue = (debtorName + " is Added! :)");
+        } else areaInfoValue = ("This Debtor arledy exist! :(");
+
+        return areaInfoValue;
+    }
+
+    public String showInfo(String debtorName) {
+
+        String areaInfoValue = "";
+
+        String dataAndDebt = "================" + "\n" +
+                "   Debt list" + "\n" +
+                "================";
+
+        DebtorDetails debtorDetails = debtorDetailsRepo.findByName(debtorName);
+
+        dataAndDebt += "\n" +
+                " ID of Debt ---->  " + debtorDetails.getId() + "\n" +
+                " Date ---> " + debtorDetails.getDate() + "\n" +
+                " Debt ---> " + debtorDetails.getDebt() + "\n" +
+                " Reason ---> " + debtorDetails.getReasonForTheDebt() +
+                "\n-----------------------------";
+
+        areaInfoValue = ("Name ---> " + debtorRepo.findByName(debtorName).getName() + "\n" +
+                "Total debt ---> " + debtorRepo.findByName(debtorName).getTotalDebt() + "\n" +
+                dataAndDebt);
+        return areaInfoValue;
+    }
+
+    @Transactional
+    public void updateDebtByNewDebt(Long debtID, float debtValue) {
+        Optional<DebtorDetails> debtorDetails = debtorDetailsRepo.findById(debtID);
+        DebtorDetails debtorDetails1 = debtorDetails.get();
+
+        float newDebt = debtorDetails1.getDebt() + debtValue;
+
+        debtorDetails1.setDebt(newDebt);
+        debtorDetailsRepo.save(debtorDetails1);
+
+        String debtorName = debtorDetailsRepo.findById(debtID).get().getName();
+        addExtraDebt(debtorName, debtValue, findUserName());
+    }
+
+    @Transactional
+    public void deleteDebtByID(String debtorName, Long debtorID, String userName) {
+        DebtorDetails debtorDetailsCopy = debtorDetailsRepo.findByNameAndId(debtorName, debtorID);
+
+        saveEntityDebtorHistory(debtorDetailsCopy);
+
+        debtorDetailsRepo.delete(debtorDetailsRepo.findByNameAndId(debtorName, debtorID));
+    }
+
+    public void saveEntityDebtorHistory(DebtorDetails debtorDetails) {
+        DebtorHistory debtorHistory = new DebtorHistory();
+        debtorHistory.setDebt(debtorDetails.getDebt());
+        debtorHistory.setName(debtorDetails.getName());
+        debtorHistory.setReasonForTheDebt(debtorDetails.getReasonForTheDebt());
+        debtorHistory.setUserName(debtorDetails.getUserName());
+
+        long daysBetween = DAYS.between(debtorDetails.getDate(), LocalDate.now());
+        debtorHistory.setTimeOfDebt(daysBetween);
+
+        debtorHistoryRepo.save(debtorHistory);
+    }
+
+    public void deleteDebtByID(Long debtorID, String userName) {
+        Optional<DebtorDetails> debtorDetailsCopyOptional = debtorDetailsRepo.findById(debtorID);
+
+        DebtorDetails debtorDetailsCopy = debtorDetailsCopyOptional.get();
+
+        saveEntityDebtorHistory(debtorDetailsCopy);
+
+        debtorDetailsRepo.deleteById(debtorID);
+    }
+
+    public void deleteFromDebtorHistoryById(Long debtorID) {
+        debtorHistoryRepo.deleteById(debtorID);
+    }
+}
