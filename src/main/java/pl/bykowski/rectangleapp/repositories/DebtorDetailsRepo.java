8c1e7fc3b0a09692ea8396ef29e5d8eceaa3d779
posionjoke <aaadik0013@gmail.com>
2019-09-14 18:36:21 +0200

    repo fix

--- src/main/java/pl/bykowski/rectangleapp/repositories/DebtorDetailsRepo.java
+++ src/main/java/pl/bykowski/rectangleapp/repositories/DebtorDetailsRepo.java
@@ -1,10 +1,12 @@
 package pl.bykowski.rectangleapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.bykowski.rectangleapp.model.DebtorDetails;
 
 import java.util.List;
 
+@Repository
 public interface DebtorDetailsRepo extends CrudRepository<DebtorDetails, Long> {
     DebtorDetails findByNameAndId(String name, Long ID);
     List<DebtorDetails> findByUserName(String userName);
--- src/main/java/pl/bykowski/rectangleapp/repositories/DebtorUserRepo.java
+++ src/main/java/pl/bykowski/rectangleapp/repositories/DebtorUserRepo.java
@@ -1,11 +1,12 @@
 package pl.bykowski.rectangleapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.bykowski.rectangleapp.model.DebtorUser;
 
 import java.util.List;
 
-
+@Repository
 public interface DebtorUserRepo extends CrudRepository<DebtorUser, Long> {
     List<DebtorUser> findByName(String name);
 }
