4a2f49c40d1351b14bfca53c09632ec3ed8e97e9
posionjoke <posionjoke@gmail.com>
2019-09-24 23:41:08 +0200

    working loggers

--- src/main/java/pl/bykowski/rectangleapp/services/NotificationService.java
+++ src/main/java/pl/bykowski/rectangleapp/services/NotificationService.java
@@ -1,5 +1,6 @@
 package pl.bykowski.rectangleapp.services;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,6 +11,8 @@ import java.util.Objects;
 
 @Service
 public class NotificationService {
+    private static final Logger logger = Logger.getLogger(NotificationService.class);
+    private static final String APP_MAIL = "rostkowskiadrian00@gmail.com";
 
     private final JavaMailSender javaMailSender;
 
@@ -23,10 +26,14 @@ public class NotificationService {
         try{
             SimpleMailMessage mail = new SimpleMailMessage();
             mail.setTo(email);
-            mail.setFrom("rostkowskiadrian00@gmail.com");
+            mail.setFrom(APP_MAIL);
             mail.setSubject("Welcome to my app :D");
             mail.setText("AuthenticationCode = " + code);
             javaMailSender.send(mail);
+            logger.debug("Mail has been sent" +
+                    "\nfrom : " + APP_MAIL +
+                    "\nto : " + email);
+
             return true;
         }catch (MailException mEx){
             mEx.printStackTrace();
