6fa5ce9ae741cec35cb84ae74cbb47afe36ad5ba
posionjoke <posionjoke@gmail.com>
2019-09-30 22:14:53 +0200

    DebtorControllerTest, update WIP

--- src/test/java/pl/bykowski/rectangleapp/controlers/DebtorControllerTest.java
+++ src/test/java/pl/bykowski/rectangleapp/controlers/DebtorControllerTest.java
@@ -17,13 +17,15 @@ import pl.bykowski.rectangleapp.services.DebtorService;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DebtorControllerTest {
@@ -50,13 +52,25 @@ public class DebtorControllerTest {
     public void should_be_status_server_ok_get_debtorList() throws Exception {
         //given
         String userName = "spring";
-        List<Debtor> debtorList = Arrays.asList();
+//        Long id = 1L;
+        String debtorName = "Adam";
+        Debtor debtor1 = new Debtor();
+        debtor1.setName(debtorName);
+        List<Debtor> debtorList = Arrays.asList(debtor1);
         given(debtorService.findByUserName(userName)).willReturn(debtorList);
 
         //when
         mvc.perform(get("/debtor-list"))
                 .andExpect(model().size(1))
                 .andExpect(model().attribute("debtors", debtorList))
+                .andExpect(model().attribute("debtors", hasSize(1)))
+                .andExpect(model().attribute("debtors", equalTo(debtorList)))
+                .andExpect(model().attribute("debtors", hasItem(
+                        allOf(
+                                hasProperty("name", is(debtorName))
+                        )
+                )))
+                .andExpect(view().name("debtor-list"))
                 .andExpect(status().isOk());
         //then
         verify(debtorService).findByUserName(userName);
