783c58526caf3148e0e2d0d72e143f28a859c0c2
posionjoke <aaadik0013@gmail.com>
2019-10-27 21:24:32 +0100

    make it better

--- src/main/java/pl/bykowski/rectangleapp/controller/DebtorDetailsController.java
+++ src/main/java/pl/bykowski/rectangleapp/controller/DebtorDetailsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.servlet.ModelAndView;
 import pl.bykowski.rectangleapp.model.Debtor;
 import pl.bykowski.rectangleapp.model.DebtorDetails;
